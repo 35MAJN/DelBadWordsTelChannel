829f7b0fe403f85324745d9c29167184220596ce
Mohammadali Javadinasab <majnnjam@gmail.com>
2023-03-29 07:44:24 +0330

    35

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import telebot
+from sansorchi import Sansorchi
 
+sansorchi = Sansorchi()
+
+# Replace YOUR_TELEGRAM_BOT_TOKEN with your actual bot token
+token = raw_input("Enter Your Bot's token:\n")
+bot = telebot.TeleBot(token)
+@bot.message_handler(func=lambda message: True)
+def check_bad_words(message):
+    text = message.text
+    if sansorchi(text):
+        bot.delete_message(message.chat.id, message.message_id)
+
+if __name__=='__main__':
+    bot.polling()
